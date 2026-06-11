a1405d13990c3df9d09df8733e1df50d77e5146c
Tanvir <tanvirahamed.foysal.00@gmail.com>
2026-06-11 21:09:21 +0600

    Make Image's links updated

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -4,19 +4,19 @@ CREATE TABLE recipe_app.foods (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     preparation_time INT,
-    calorie INT,
+    calories INT,
     category VARCHAR(10), --  breakfast, lunch, dinner
     is_favorite BOOLEAN DEFAULT FALSE,
-    rating FLOAT,
+    rating FLOAT DEFAULT 0.0,
     total_reviews INT,
-    photo_url VARCHAR(255)
+    photo_url VARCHAR(512)
 );
 
 CREATE TABLE recipe_app.ingredients (
     id SERIAL PRIMARY KEY,
     food_id INT REFERENCES recipe_app.foods(id) ON DELETE CASCADE,
     name VARCHAR(100) NOT NULL,
-    quantity VARCHAR(50) NOT NULL,
-    photo_url VARCHAR(255)
+    quantity FLOAT NOT NULL,
+    photo_url VARCHAR(512)
 );
 
